bc70155651dd3bdec714cd2565ecb93403826995
luecha.kan <luecha.kanm@gmail.com>
2025-02-08 15:50:11 +0700

    remove comment

--- models/verify_email.go
+++ models/verify_email.go
@@ -1,14 +1,5 @@
 package models
 
-// type VerifyEmailRequest struct {
-// 	EmailId    int64  `json:"email_id,omitempty"`
-// 	SecretCode string `json:"secret_code,omitempty"`
-// }
-
-// type VerifyEmailResponse struct {
-// 	IsVerified bool `json:"is_verified,omitempty"`
-// }
-
 type VerifyEmailRequest struct {
 	EmailId    int    `form:"email_id" binding:"required"`
 	SecretCode string `form:"secret_code" binding:"required"`
